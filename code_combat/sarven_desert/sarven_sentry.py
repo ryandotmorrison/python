92ad5383553cb262c9099a389daa9f4a77ea6b69
Ryan <150568593+ryandotmorrison@users.noreply.github.com>
2024-01-28 12:00:48 -0800

    Create sarven_sentry.py

---
+++ code_combat/sarven_desert/sarven_sentry.py
@@ -0,0 +1,24 @@
+# Use different colored flags to perform different tasks.
+
+while True:
+    flagGreen = hero.findFlag("green")
+    flagBlack = hero.findFlag("black")
+    
+    # If there's a flagGreen...
+    if flagGreen:
+        
+        # Build a "fence" at flagGreen's position.
+        hero.buildXY("fence", flagGreen.pos.x, flagGreen.pos.y)
+        # Pick up the flag!
+        hero.findFlag("green")
+        hero.pickUpFlag(flagGreen)
+    # If there's a flagBlack...
+    if flagBlack:
+        
+        # Build a "fire-trap" at flagBlack's position.
+        hero.buildXY("fire-trap", flagBlack.pos.x, flagBlack.pos.y)
+        # Pick up the flag!
+        hero.findFlag("black")
+        hero.pickUpFlag(flagBlack)
+    # Move back to the center.
+    hero.moveXY(43, 31)
