e36768dc2a2c3a1d50fdf88b7ed8d4e80765ceff
Ryan <150568593+ryandotmorrison@users.noreply.github.com>
2024-01-28 11:04:38 -0800

    Create pong_pong.py

---
+++ code_combat/kithgard_dungeon/pong_pong.py
@@ -0,0 +1,10 @@
+# Use all the coding skills you've learned so far to keep the balls from hitting the ground!
+# You can hit the balls into the air with the attack() method.
+# For a challenge, try to use fewer lines of code.
+while True:
+    hero.attack("ball")
+    hero.attack("ball2")
+    hero.moveRight(3)
+    hero.attack("ball")
+    hero.attack("ball2")
+    hero.moveLeft(3)
