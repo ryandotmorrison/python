ffc9e147348a29897c43203469feb94031c63f9c
Ryan <150568593+ryandotmorrison@users.noreply.github.com>
2024-01-28 10:57:35 -0800

    Create sleeping_on_the_job.py

---
+++ code_combat/kithgard_dungeon/sleeping_on_the_job.py
@@ -0,0 +1,11 @@
+# The guards protect the buttons to the door.
+hero.moveUp(1)
+# Move between the sleeping ogres:
+hero.moveRight(2)
+# Defeat the ogres by attacking them twice:
+hero.attack("Bas")
+hero.attack("Bas")
+hero.attack("Vera")
+hero.attack("Vera")
+# Move to the gem and escape:
+hero.moveRight(2)
