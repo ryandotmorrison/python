0cc753a6995c3e1046bb484b0100cb51b7db2465
Ryan <150568593+ryandotmorrison@users.noreply.github.com>
2024-01-28 11:52:49 -0800

    Create bank_raid.py

---
+++ code_combat/sarven_desert/bank_raid.py
@@ -0,0 +1,24 @@
+# Wait for ogres, defeat them and collect gold.
+
+while True:
+    enemies = hero.findEnemies()
+    # enemyIndex is used to iterate the enemies array.
+    enemyIndex = 0
+    # While enemyIndex is less than len(enemies)
+    while enemyIndex < len(enemies):
+        # Attack the enemy at enemyIndex
+        enemy = enemies[enemyIndex]
+        hero.attack(enemy)
+        # Increase enemyIndex by one.
+        enemyIndex += 1
+    coins = hero.findItems()
+    # coinIndex is used to iterate the coins array.
+    coinIndex = 0
+    while coinIndex < len(coins):
+        coin = hero.findNearestItem()
+        # Get a coin from the coins array using coinIndex
+        
+        # Collect that coin.
+        hero.moveXY(coin.pos.x, coin.pos.y)
+        # Increase coinIndex by one.
+        coinIndex += 1
