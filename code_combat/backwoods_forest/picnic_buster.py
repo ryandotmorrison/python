4eb8e543cce93a2f7a566d11a210130c058db74e
Ryan <150568593+ryandotmorrison@users.noreply.github.com>
2024-01-28 11:41:42 -0800

    Create picnic_buster.py

---
+++ code_combat/backwoods_forest/picnic_buster.py
@@ -0,0 +1,6 @@
+# Remember that enemies may not yet exist.
+while True:
+    enemy1 = hero.findNearestEnemy()
+    if enemy1:
+        # If there is an enemy, attack it!
+        hero.attack(enemy1)
