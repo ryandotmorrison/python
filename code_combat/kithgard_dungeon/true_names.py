8a7225871c73a0fd111003d4f218059ac228dea9
Ryan <150568593+ryandotmorrison@users.noreply.github.com>
2024-01-28 10:59:29 -0800

    Create true_names.py

---
+++ code_combat/kithgard_dungeon/true_names.py
@@ -0,0 +1,10 @@
+# Defend against "Brak" and "Treg"!
+# You must attack small ogres twice.
+
+hero.moveRight()
+hero.attack("Brak")
+hero.attack("Brak")
+hero.moveRight(2)
+hero.attack("Treg")
+hero.attack("Treg")
+hero.moveRight(2)
