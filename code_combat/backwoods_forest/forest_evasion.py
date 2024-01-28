f8e417e1a70d781adf1d8b1665271e6ff0cb1dd1
Ryan <150568593+ryandotmorrison@users.noreply.github.com>
2024-01-28 11:42:02 -0800

    Create forest_evasion.py

---
+++ code_combat/backwoods_forest/forest_evasion.py
@@ -0,0 +1,13 @@
+# There is a headhunter in the area!
+# Move around the forest to avoid his line-of-sight.
+
+while True:
+    hero.moveXY(56, 40)
+    # Use moveXY to keep moving around the forest to survive.
+    # Don't forget about indents.
+    hero.moveXY(40, 56)
+    hero.moveXY(24,44)
+    hero.moveXY(24,24)
+    hero.moveXY(40,12)
+    hero.moveXY(56,24)
+    hero.moveXY(56,40)
