e0ff2c9fb5c5fd232dec39857232bacffea24d4a
Ryan <150568593+ryandotmorrison@users.noreply.github.com>
2024-01-28 11:03:15 -0800

    Create loop_da_loop.py

---
+++ code_combat/kithgard_dungeon/loop_da_loop.py
@@ -0,0 +1,11 @@
+# Code inside of a while-true loop will repeat forever!
+
+while True:
+    # Move right
+    hero.moveRight()
+    # Move up
+    hero.moveUp(1)
+    # Move left
+    hero.moveLeft(1)
+    # Move down
+    hero.moveDown(1)
